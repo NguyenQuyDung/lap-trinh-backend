9d1bfd156e1abae4cccedfac0d9872f8de90e43d
NguyenQuyDung <103623056+NguyenQuyDung@users.noreply.github.com>
2023-12-05 09:48:02 +0700

    khoa-hoc-lap-trinh-php

---
+++ JS/body_group.js
@@ -0,0 +1,16 @@
+
+  $('.lecturects-group').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    wrapAround:true,
+    autoPlay:5000,
+  });
+  $('.students-group').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    wrapAround:true,
+    autoPlay:5000,
+  });
+  
